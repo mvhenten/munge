f66137001cb82065e3912b91597a59cdda3dff01
Matthijs van Henten <mvhenten@tty.nl>
2012-09-24 14:09:58 +0200

    updated dependencies

--- scripts/scandeps.sh
+++ scripts/scandeps.sh
@@ -1,4 +1,4 @@
 git grep -h -E '^\s+?use [[:alnum:]:]+;' | \
 grep -v -E 'use [strict|utf8|warnings]' | \
 grep -v 'Munge' | \
-sed -E 's/\s+?use(.+);/\1/g'
+sed -E 's/\s+?use\s+(.+);/\1/g' | sort | uniq
