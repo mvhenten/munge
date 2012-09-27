4cc640679626141db47f53e0809e763a3342e1da
Matthijs van Henten <matthijs@ischen.nl>
2012-09-27 21:47:51 +0200

    adding config

--- Munge-App/lib/Munge/Config.pm
+++ Munge-App/lib/Munge/Config.pm
@@ -1,5 +1,20 @@
 package Munge::Config;
 
+use YAML::Any qw|LoadFile|;
+
+{
+    my $config;
+
+    sub config {
+        return $config ||= LoadFile('config.yml');
+    }
+
+}
+
+sub DSN {
+    return config()->{plugins}->{DBIC}->{dsn};
+}
+
 #use MooseX::Declare;
 
 #class Munge::Model::Feed::Parser {
---
+++ Munge-App/t/Munge/Config.pm
@@ -0,0 +1,12 @@
+use MooseX::Declare;
+
+class t::Munge::Config {
+    use Munge::Config;
+    use Data::Dumper;
+    use Test::Sweet;
+    
+    test basic {
+        warn Dumper( Munge::Config::config() );
+    }
+
+}
