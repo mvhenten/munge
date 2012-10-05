19dd30941ca74bbf74f544f8e4efd2d350480409
Matthijs van Henten <matthijs@ischen.nl>
2012-10-05 23:24:11 +0200

    dev server runner

---
+++ scripts/start-dev-server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PROGRAM='./bin/app.pl'
+
+$PROGRAM &
+
+terminate_pid () {
+    for i in `pgrep -f "$PROGRAM"`; do
+        echo "TERMINATE $PROGRAM $i";
+        kill $i;
+        wait $i;
+   done;
+}
+
+while inotifywait -e modify -r lib/Munge/;
+    do terminate_pid; sleep 1 && $PROGRAM &
+done;
