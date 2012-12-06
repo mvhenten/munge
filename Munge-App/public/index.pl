61989332ef59223817266aa717dd091a923d2ea8
Matthijs van Henten <matthijs@ischen.nl>
2012-12-06 23:46:37 +0100

    Revert "trying to get things working"
    This reverts commit 996ef43aecbcefa67f8866c543bc5d041c1bc4e8.

--- Munge-App/public/index.pl
+++ Munge-App/public/index.pl
@@ -1,1 +1,14 @@
-dispatch.cgi
+#!/usr/bin/env perl
+use Dancer ':syntax';
+use Plack::Runner;
+
+# For some reason Apache SetEnv directives dont propagate
+# correctly to the dispatchers, so forcing PSGI and env here 
+# is safer.
+set apphandler => 'PSGI';
+set environment => 'production';
+
+my $psgi = path($ENV{'DOCUMENT_ROOT'}, '..', 'Munge-App', 'bin', 'app.pl');
+die "Unable to read startup script: $psgi" unless -r $psgi;
+
+Plack::Runner->run($psgi);
