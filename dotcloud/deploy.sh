cd69684679db888e4c2fcb83d6e9cc23747cb237
Matthijs van Henten <matthijs@ischen.nl>
2013-01-02 01:25:30 +0100

    improve deploy script

--- dotcloud/deploy.sh
+++ dotcloud/deploy.sh
@@ -1,6 +1,9 @@
 rm -rf App
-CURDIR=cwd
+rm -rf static
 cd ../Munge-App/
 dzil build
 cd ../dotcloud/
 cp -r ../Munge-App/Munge-App-0.01 App
+rm -rf ../Munge-App/Munge-App-0.01*
+cp -r App/public static
+dotcloud push
