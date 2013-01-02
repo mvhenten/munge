fbe05226bdeceb8b08e00fd2ca604ac0cf4fd478
Matthijs van Henten <mvhenten@tty.nl>
2013-01-02 12:47:40 +0100

    symlink the static dir

--- dotcloud/deploy.sh
+++ dotcloud/deploy.sh
@@ -1,9 +1,8 @@
 rm -rf App
-rm -rf static
 cd ../Munge-App/
 dzil build
 cd ../dotcloud/
 cp -r ../Munge-App/Munge-App-0.01 App
 rm -rf ../Munge-App/Munge-App-0.01*
-cp -r App/public static
+ln -s public App/static
 dotcloud push
