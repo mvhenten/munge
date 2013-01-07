15289500c8caee4cb8be034c7ec6e6e7c582affc
Matthijs van Henten <matthijs@ischen.nl>
2013-01-07 23:04:30 +0100

    forgo on connection re-using for the time being

--- Munge-App/lib/Munge/DBIC.pm
+++ Munge-App/lib/Munge/DBIC.pm
@@ -17,32 +17,29 @@ use strict;
 use warnings;
 
 {
-    my $schema;
 
     sub get_connection {
-        if ( not $schema ) {
-            $schema = Munge::Schema->connect( sub {
-                my $conn = DBIx::Connector->new(
-                    Munge::Config::DSN(),
-                    Munge::Config::DB_USER(),
-                    Munge::Config::DB_PASSWORD(),
-                    {
-                        RaiseError        => 1,
-                        AutoCommit        => 1,
-                        mysql_enable_utf8 => 1,
-                        quote_names       => 1
-                    }
-                );
+        my $conn = DBIx::Connector->new(
+            Munge::Config::DSN(),
+            Munge::Config::DB_USER(),
+            Munge::Config::DB_PASSWORD(),
+            {
+                RaiseError        => 1,
+                AutoCommit        => 1,
+                mysql_enable_utf8 => 1,
+                quote_names       => 1
+            }
+        );
 
+        return Munge::Schema->connect(
+            sub {
                 return $conn->dbh;
             },
             {
                 mysql_enable_utf8 => 1,
-                quote_names => 1
-            });
-        }
-
-        return $schema;
+                quote_names       => 1
+            }
+        );
     }
 }
 
