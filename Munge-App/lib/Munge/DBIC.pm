04c7d6e3d7300ce38de607691fa2a8bceed68554
Matthijs van Henten <mvhenten@tty.nl>
2013-01-07 18:21:51 +0100

    final attempt fixing "gone away bug"

--- Munge-App/lib/Munge/DBIC.pm
+++ Munge-App/lib/Munge/DBIC.pm
@@ -18,28 +18,38 @@ use warnings;
 
 {
     my $schema;
+    my $conn;
+
+    sub get_connector {
+        return DBIx::Connector->new(
+            Munge::Config::DSN(),
+            Munge::Config::DB_USER(),
+            Munge::Config::DB_PASSWORD(),
+            {
+                RaiseError        => 1,
+                AutoCommit        => 1,
+                mysql_enable_utf8 => 1,
+                quote_names       => 1
+            }
+        );
+    }
 
     sub get_connection {
         if ( not $schema ) {
             $schema = Munge::Schema->connect( sub {
-                my $conn = DBIx::Connector->new(
-                    Munge::Config::DSN(),
-                    Munge::Config::DB_USER(),
-                    Munge::Config::DB_PASSWORD(),
-                    {
-                        RaiseError        => 1,
-                        AutoCommit        => 1,
-                        mysql_enable_utf8 => 1,
-                        quote_names       => 1
-                    }
-                );
+                if( $conn && $conn->connected ) {
+                    return $conn->dbh;
+                }
 
-                return $conn->dbh;
-            },
-            {
-                mysql_enable_utf8 => 1,
-                quote_names => 1
-            });
+                $conn = get_connector();
+
+                my $dbh = $conn->dbh;
+
+                return $dbh;
+            }, {
+                        quote_names       => 1
+
+            } );
         }
 
         return $schema;
