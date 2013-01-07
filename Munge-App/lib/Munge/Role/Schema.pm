706697acaab4ae8762d5c56da8bd64bb47139033
Matthijs van Henten <matthijs@ischen.nl>
2013-01-07 23:17:11 +0100

    no need for package DBIC anymore

--- Munge-App/lib/Munge/Role/Schema.pm
+++ Munge-App/lib/Munge/Role/Schema.pm
@@ -3,7 +3,6 @@ use MooseX::Declare;
 role Munge::Role::Schema {
     use Munge::Config;
     use Munge::Schema;
-    use Munge::DBIC;
 
     has schema => (
         is         => 'ro',
@@ -13,6 +12,26 @@ role Munge::Role::Schema {
     );
 
     method _build_schema {
-        return Munge::DBIC::get_connection();
+        my $conn = DBIx::Connector->new(
+            Munge::Config::DSN(),
+            Munge::Config::DB_USER(),
+            Munge::Config::DB_PASSWORD(),
+            {
+                RaiseError        => 1,
+                AutoCommit        => 1,
+                mysql_enable_utf8 => 1,
+                quote_names       => 1
+            }
+        );
+
+        return Munge::Schema->connect(
+            sub {
+                return $conn->dbh;
+            },
+            {
+                mysql_enable_utf8 => 1,
+                quote_names       => 1
+            }
+        );
     }
 }
