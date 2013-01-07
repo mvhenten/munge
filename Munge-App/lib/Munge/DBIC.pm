4524d46c7bc6f0627e907c4ee4878aa3dd67a575
Matthijs van Henten <mvhenten@tty.nl>
2013-01-07 17:37:24 +0100

    hopefully fixed mysql has gone away error

--- Munge-App/lib/Munge/DBIC.pm
+++ Munge-App/lib/Munge/DBIC.pm
@@ -21,20 +21,25 @@ use warnings;
 
     sub get_connection {
         if ( not $schema ) {
-            my $conn = DBIx::Connector->new(
-                Munge::Config::DSN(),
-                Munge::Config::DB_USER(),
-                Munge::Config::DB_PASSWORD(),
-                {
-                    RaiseError        => 1,
-                    AutoCommit        => 1,
-                    mysql_enable_utf8 => 1,
-                    quote_names       => 1
-                }
-            );
+            $schema = Munge::Schema->connect( sub {
+                my $conn = DBIx::Connector->new(
+                    Munge::Config::DSN(),
+                    Munge::Config::DB_USER(),
+                    Munge::Config::DB_PASSWORD(),
+                    {
+                        RaiseError        => 1,
+                        AutoCommit        => 1,
+                        mysql_enable_utf8 => 1,
+                        quote_names       => 1
+                    }
+                );
 
-            $schema = Munge::Schema->connect( sub { return $conn->dbh },
-                { mysql_enable_utf8 => 1, quote_names => 1 } );
+                return $conn->dbh
+            },
+            {
+                mysql_enable_utf8 => 1,
+                quote_names => 1
+            });
         }
 
         return $schema;
--- Munge-App/lib/Munge/Storage.pm
+++ Munge-App/lib/Munge/Storage.pm
@@ -17,7 +17,7 @@ Takes a class, stores it's public parts
 
 class Munge::Storage {
     use Data::Dumper;
-    
+
     with 'Munge::Role::Schema';
 
     has account => (
@@ -25,18 +25,18 @@ class Munge::Storage {
         isa      => 'Munge::Schema::Result::Account',
         required => 1,
     );
-    
+
     has schema_name => (
         is       => 'ro',
         isa      => 'Str',
-        required => 1,    
+        required => 1,
     );
 
     has _schema_class => (
         is => 'ro',
         lazy_build => 1,
     );
-    
+
     method _build__schema_class {
         my $name = $self->schema_name;
         return $name->new();
@@ -44,44 +44,44 @@ class Munge::Storage {
 
     method store ( $object ) {
         my $values = $self->_storable_attributes( $object );
-        
+
         $values->{account_id} = $self->account->id;
-        
+
         my $rs = $self->resultset( $self->schema_name )->update_or_create( $values );
         return $rs->get_inflated_columns();
     }
-    
+
     method load ( $key, $value ) {
         my ( $result ) = $self->_find( $key, $value );
-                        
+
         return defined( $result ) ?  { $result->get_inflated_columns() } : undef;
     }
-    
+
     method delete ( $key, $value ) {
         my ( $result ) = $self->_find( $key, $value );
-               
+
         for my $relation ($result->relationships ) {
             my $info = $result->relationship_info( $relation );
-            
-            
+
+
             if( $info->{attrs}->{cascade_delete} ){
                 $result->$relation()->delete();
             }
         }
-        
+
         $result->delete() if defined( $result );
 
         return;
     }
-        
+
     method _find ( $key, $value ) {
         my ( $rs ) = $self->resultset( $self->schema_name )->search( { $key => $value, account_id => $self->account->id } );
         return $rs;
     }
- 
+
     method _storable_attributes ( Object $object ) {
         my %storable_attributes;
-        
+
         for my $key ( $self->_schema_class->columns() ){
             next if not $object->can( $key );
 
