998ce59e766687426ec83071c502c499a12ce533
Matthijs van Henten <mvhenten@tty.nl>
2013-01-09 09:19:37 +0100

    checkout from origin, do not use our version

--- Munge-App/lib/Munge/Role/Schema.pm
+++ Munge-App/lib/Munge/Role/Schema.pm
@@ -3,16 +3,37 @@ use MooseX::Declare;
 role Munge::Role::Schema {
     use Munge::Config;
     use Munge::Schema;
-    use Munge::DBIC;
 
-    has schema => (
-        is         => 'ro',
-        isa        => 'Munge::Schema',
-        lazy_build => 1,
-        handles    => ['resultset'],
-    );
+    {
+        my $schema;
 
-    method _build_schema {
-        return Munge::DBIC::get_connection();
+        sub resultset {
+            my ( $self, @args ) = @_;
+
+            return $self->schema->resultset(@args);
+        }
+
+        method schema {
+            if( $schema and not $schema->storage->connected ){
+                $schema = undef;
+            }
+            
+            if ( not $schema ) {
+                $schema = Munge::Schema->connect(
+                    Munge::Config::DSN(),
+                    Munge::Config::DB_USER(),
+                    Munge::Config::DB_PASSWORD(),
+                    {
+                        RaiseError        => 1,
+                        AutoCommit        => 1,
+                        mysql_enable_utf8 => 1,
+                        quote_names       => 1
+                    }
+                );
+
+            }
+
+            return $schema;
+        }
     }
 }
--- Munge-App/lib/Munge/Role/Storage.pm
+++ Munge-App/lib/Munge/Role/Storage.pm
@@ -7,9 +7,9 @@ role Munge::Role::Storage {
     use Munge::Types qw|UUID|;
     use Carp::Assert;
     use Data::Dumper;
-
+    
     requires qw|account schema|;
-
+    
     has _storage => (
         is     => 'ro',
         isa    => 'Munge::Storage',
@@ -18,57 +18,45 @@ role Munge::Role::Storage {
 
     sub NAMESPACE_PREFIX {
         return 'Munge::Schema::Result';
-    }
-
+    }    
+    
     method _build__storage {
         $self->_get_storage( $self->account );
     }
-
-    method store {
-        my %values;
-
-        if( $self->id ){
-            $self->_storage->update( $self )
-        }
-        else {
-            $self->_storage->create( $self )
-        }
-
-        foreach my $key ( keys %values ){
-            my $method = "_set_$key";
-
-            next if not $self->can( $method );
-            $self->$method( $values{$key} );
-        }
+    
+    method store {        
+        my %values = $self->id ? $self->_storage->update( $self ) : $self->_storage->create( $self );
+                
+        return $self->new(  %values, account => $self->account );
     }
-
+     
     method delete {
         $self->_storage->delete( uuid => $self->uuid );
     }
-
-    method load ( $class: UUID $uuid, Account $account, $storage? ){
+    
+    method load ( $class: UUID $uuid, Account $account, $storage? ){        
         $storage ||= $class->_get_storage( $account );
-
-        my $rs = $storage->load( uuid => $uuid );
+                        
+        my $rs = $storage->load( uuid => $uuid );        
         return if not $rs;
-
+                
         delete( $rs->{account_id} );
         my @keys = grep { defined( $rs->{$_} ) } keys %{ $rs };
-
+        
         delete( $rs->{@keys} );
-
+        
         return $class->new(  %{ $rs }, account => $account );
     }
-
+                
     method _schema_class ( $class: ){
         my ( $class_name ) = ( ref $class  || $class ) =~ m/.+::(\w+)/;
-
+                
         return NAMESPACE_PREFIX() . "::$class_name";
     }
-
+    
     method _get_storage ( $class: Account $account ) {
-        return Munge::Storage->new( schema_name => $class->_schema_class(), account => $account );
+        return Munge::Storage->new( schema_name => $class->_schema_class(), account => $account );                
     }
-
-
+    
+    
 }
