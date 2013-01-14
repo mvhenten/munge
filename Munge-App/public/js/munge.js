77e81d2056e4ba657ab0e0bcea98f5fff2900bba
Matthijs van Henten <matthijs@ischen.nl>
2013-01-14 01:00:47 +0100

    minor js hack to remove broken images

--- Munge-App/public/js/munge.js
+++ Munge-App/public/js/munge.js
@@ -1,6 +1,24 @@
 $( document ).ready( function(){
     /* remove broken images */
-    $('img').each( function(){ if( ! this.src ) { $(this).remove(); } });
+    $('img').each( function(){
+        if( ! this.src ) {
+            $(this).remove();
+            return;
+        }
+        
+        
+        var img = new Image(), orig = this;
+        
+        $(img).load( function(){
+            /** if it can be loaded, its not pix > 1 **/
+            if( img.width > 1 ){
+                $(orig).show();                
+            }
+        });
+        
+        img.src = this.src;
+        $(orig).hide();
+    });
     
     /* open links from articles in a new window */
     $('.feed-content a').each(function(){
