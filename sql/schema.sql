bb69e60d0d5c9b276938abdf9889bf957a6eaa13
Matthijs van Henten <matthijs@ischen.nl>
2013-01-20 00:04:58 +0100

    new schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,52 +9,63 @@ CREATE TABLE `account` (
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `account` VALUES (1,'mvhenten@tty.nl','{SSHA}KT4YUtso4IxjwdRFFB3yAtFV6zDrv0so','','0000-00-00 00:00:00','2012-10-09 08:13:03'),(2,'test@ischen.nl','{SSHA}75sedvBEZPrJr6avd4+F4T6sX19lGE9H','','0000-00-00 00:00:00','2012-10-28 21:23:49');
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account_feed` (
+  `account_id` int(10) unsigned NOT NULL,
+  `feed_uuid` binary(16) NOT NULL,
+  PRIMARY KEY (`account_id`,`feed_uuid`),
+  KEY `account_key` (`account_id`),
+  CONSTRAINT `account_fk` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account_feed_item` (
+  `account_id` int(10) unsigned NOT NULL,
+  `feed_item_uuid` binary(16) NOT NULL,
+  `feed_uuid` binary(16) NOT NULL,
+  `read` tinyint(2) NOT NULL DEFAULT '0',
+  `starred` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`account_id`,`feed_uuid`,`feed_item_uuid`),
+  KEY `feed_uuid_fk` (`feed_uuid`),
+  KEY `read_idx` (`read`),
+  KEY `star_idx` (`starred`),
+  CONSTRAINT `feed_uuid_fk` FOREIGN KEY (`feed_uuid`) REFERENCES `feed` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `feed` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `account_id` int(10) unsigned NOT NULL,
   `link` varchar(2048) NOT NULL,
   `title` varchar(512) NOT NULL DEFAULT '',
   `description` varchar(4096) NOT NULL DEFAULT '',
   `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `uuid` binary(16) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `account_uuid_idx` (`account_id`,`uuid`),
-  KEY `index_updated` (`updated`),
-  KEY `account_id_fk` (`account_id`),
-  CONSTRAINT `account_id_fk` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+  PRIMARY KEY (`uuid`),
+  KEY `index_updated` (`updated`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `feed_item` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `feed_id` int(10) unsigned NOT NULL,
-  `account_id` int(10) unsigned NOT NULL,
   `uuid` binary(16) NOT NULL,
   `link` varchar(2048) NOT NULL,
   `author` varchar(2048) NOT NULL DEFAULT '',
   `title` varchar(512) NOT NULL DEFAULT '',
   `tags` varchar(1024) NOT NULL DEFAULT '',
-  `summary` text(4096),
-  `content` text(32768),
-  `issued` datetime  DEFAULT '0000-00-00 00:00:00',
-  `modified` datetime  DEFAULT '0000-00-00 00:00:00',
-  `read` tinyint(2) NOT NULL DEFAULT '0',
-  `starred` int(11) NOT NULL DEFAULT '0',
+  `summary` text,
+  `content` mediumtext,
+  `issued` datetime DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime DEFAULT '0000-00-00 00:00:00',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `account_feed_uuid` (`uuid`,`account_id`),
-  KEY `account_id_fk` (`account_id`),
-  KEY `feed_id_fk` (`feed_id`),
-  KEY `read_idx` (`read`),
-  KEY `star_idx` (`starred`),
-  CONSTRAINT `feed_id_fk` FOREIGN KEY (`feed_id`) REFERENCES `feed` (`id`),
-  CONSTRAINT `item_account_id_fk` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+  `feed_uuid` binary(16) DEFAULT NULL,
+  `poster_image` varchar(2048) NOT NULL DEFAULT '',
+  PRIMARY KEY (`uuid`),
+  KEY `feed_uuid_key` (`feed_uuid`),
+  CONSTRAINT `feed_item_ibfk_1` FOREIGN KEY (`feed_uuid`) REFERENCES `feed` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
